63527275e04a2577ae71df4b70c66cf251466826
bellachen00 <bchen76@ucsc.edu>
2025-05-10 02:31:06 -0700

    small edit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import NavBar from './components/NavBar';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -20,7 +20,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
       <NavBar/>
     </div>
   );
