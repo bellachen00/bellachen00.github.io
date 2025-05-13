a6d40c27e7343506c8da04039423d0c61ca25923
bellachen00 <bchen76@ucsc.edu>
2025-05-13 08:19:00 -0700

    created contact file

---
+++ src/components/Contact.jsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { Link, List, ListItem, ListItemDecorator, Stack, Typography } from '@mui/joy';
+import EmailRoundedIcon from '@mui/icons-material/EmailRounded';
+import LocationCityRoundedIcon from '@mui/icons-material/LocationCityRounded';
+import WebRoundedIcon from '@mui/icons-material/WebRounded';
+
