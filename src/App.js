dba7c4f56507568e06fb0451d9b308b1bc645d7a
bellachen00 <bchen76@ucsc.edu>
2025-05-21 02:12:00 -0700

    edits

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NavBar from './components/NavBar';
 import About from './components/About';
 import Resume from './components/Resume';
 import Contact from './components/Contact';
+import Projects from './components/Projects';
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
       <NavBar/>
       <Divider/>
       <div id="About"><About/></div>
+      <Box>
+        <Projects/>
+      </Box>
       <Stack direction="row" p={2} spacing={2}>
         <div id="Resume"><Resume/></div>
         <div id="Contact"><Contact/></div>
---
+++ src/components/Projects.jsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import {
+    Box,
+    Button,
+    Card,
+    Divider,
+    Grid,
+    Link,
+    List,
+    ListItemDecorator,
+    ListItem,
+    Stack,
+    Typography,
+  } from '@mui/joy';
+import { Description } from '@mui/icons-material';
+
+  const myProjects = [
+    {
+        title: "Multi-Threaded HTTP Server",
+        link: "",
+        desc: [""],
+    },
+    {
+        title: "Javascript Calendar",
+        link: "https://github.com/bellachen00/Javascript-Calendar",
+        desc: ['Created a program that has a function of a calendar visually which allows us to move between the previous and next months',
+            'Highlighted the current day by mapping its value to an identification labeled ”today”'
+        ],
+    },
+    {
+        title: "Gmail Clone",
+        link: "",
+        desc: [""],
+    },
+  ];
+
+  export default function Projects() {
+    return (
+        <Stack direction={'column'}>
+            <Typography>
+                Projects
+            </Typography>
+            <Grid container spacing={1} p={0}>
+                {myProjects.map(project => (
+                <List xs={12}>
+                    <Link href={project.link} >
+                        {project.title}
+                    </Link>
+                    <Stack spacing={1}>
+                        {project.desc.map((line, i) => (
+                        <Typography key={i} variant="body2" color="text.secondary">
+                            {line}
+                        </Typography>
+                        ))}
+                    </Stack>
+                </List>
+                ))}
+            </Grid>
+        </Stack>
+    );
+  }
